386400e9d72cc89d6483c321bc61f38ad5ffb009
des-ant <57375593+des-ant@users.noreply.github.com>
2021-11-09 12:09:41 +1100

    Fixed the bug so that after multiple votes in a row, the notification for the last vote is displayed for five seconds. -ex6.21

--- redux-anecdotes/src/reducers/notificationReducer.js
+++ redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,4 +1,7 @@
 const initialState = null;
+// Identifies timer created by the call to setTimeout()
+// Will be passed to clearTimeout() to cancel the timeout on new notification
+let timeoutID = null;
 
 const notificationReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -13,13 +16,17 @@ const notificationReducer = (state = initialState, action) => {
 
 export const setNotification = (message, durationInSeconds) => {
   return async dispatch => {
+    // Cancel removal of previous notification when a new notification is displayed
+    if (timeoutID !== null) {
+      clearTimeout(timeoutID);
+    }
     dispatch({
       type: 'SET_NOTIFICATION',
       data: {
         message,
       },
     });
-    setTimeout(() => {
+    timeoutID = setTimeout(() => {
       dispatch({
         type: 'CLEAR_NOTIFICATION',
         data: null,
